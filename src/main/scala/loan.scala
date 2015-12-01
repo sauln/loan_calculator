2145235b80a9f812db3870a27efc85f16f161f75
Nathaniel Saul <nat@saulgill.com>
2015-11-30 21:38:24 -0800

    developed structure of strategy and loans

--- src/main/scala/loan.scala
+++ src/main/scala/loan.scala
@@ -1,44 +1,120 @@
 package loan
 
+
 /** 
  * Do I write it first, and then make it functional?
  * Or do I write it functional from the get go?  
  */
 
-class strategy{
-  /* how do these pieces actually fit together?
-   */
+
+/**
+ * end the end, we want a list of (loan, payment) tuples so we can show a table
+ * of all the values that we come up with.
+ *
+ */
 
 
+//want loans, payments  - should there be an object that is (loan, payment)??
 
+object Strategy{
 
+  type Strat =  (List[Loan], Double) => List[(Double, Loan)]
+
+  def minimum: Strat = {
+    case (loans: List[Loan], b: Double) =>
+ // def minimum(loans: List[Loan], available_payment: Double): List[(Double, Loan)] = {
+       loans.map(_.min) zip loans
+  }
+  def even(loans: List[Loan], available_payment: Double): List[(Double, Loan)] = {
+      ((loans.map(_.min) zip
+      List.fill(loans.length)(available_payment/loans.length))
+      .map{ case(a,b) => a max b }) zip loans
+  }
+  def snowball(loans: List[Loan], available_payment: Double): List[(Double, Loan)] = {
+      loans.map(_.min) zip loans
+  }
 }
 
 
 
 
-class loan(interest_rate: Double, principal: Double, minimum_payment: Double){
-  var current_balance = principal
-  var interest = interest_rate  
 
-  def pay(payment: Double){ 
-    current_balance -= payment 
+
+class Loan(i: Double, b: Double, m: Double){
+  val min = m
+  val balance = b
+  val interest = i
+
+  def make_payment(payment: Double): Loan = {
+    val new_balance = balance - payment
+    val current_Loan = new Loan(interest, new_balance, min)
+    current_Loan
   }
-
-  def accrue_interest(time_since: Double){ 
-    current_balance += current_balance * (1 + time_since * interest_rate)
+  def accrue_interest(months: Int = 1): Loan = {
+    val interest_paid = balance * ( 1 + (interest * months)/12)
+    val current_Loan = new Loan(interest, balance - interest_paid, min)
+    current_Loan
   }
-
-
-
-
 }
 
+/*
+trait Loan{
+  def make_payment(payment: Double): Loan
+  def accrue_interest(months: Int): Loan
+}
+*/
+/*
+object Loan {
+  case class Student(interest: Double, balance: Double, min: Double)
+    extends Loan {
+    def make_payment(payment: Double): Loan = {
+      val new_balance = balance - payment
+      val current_Loan = Student(interest, new_balance, min)
+      current_Loan
+    }
+    def accrue_interest(months: Int = 1): Loan = {
+      val interest_paid = balance * ( 1 + (interest * months)/12)
+      val current_Loan = Student(interest, balance - interest_paid, min)
+      current_Loan
+    }
+  }
+
+}
+*/
 
 
 
 
 
+
+//class Loan(interest_rate: Double, principal: Double, min: Double){
+  //var current_balance = principal
+  //var interest = interest_rate 
+  //val minimum_payment = min
+
+  //I think this is a bad way to do it.
+  //def pay(payment: Double): Loan =  { 
+  //  current_balance -= payment
+  //  this
+  //}
+
+  //def accrue_interest(time_since: Double) { 
+  //  current_balance += current_balance * (1 + time_since * interest_rate)
+  //}
+
+
+
+
+
+//object Loan{
+
+  //def pay_new(ln: Loan, payment: Double): Loan = {
+  //  val a = new Loan(ln.interest, ln.current_balance - payment, ln.minimum_payment)
+  //  a
+  //}
+
+
+//}
 
 
 
--- src/main/scala/manage.scala
+++ src/main/scala/manage.scala
@@ -7,8 +7,8 @@
 
 /**
  * want:  
- *  create a handful of loans
- *  send stack of loans to optimization algorithm
+ *  create a handful of Loans
+ *  send stack of Loans to optimization algorithm
  *  return is the best pay given the strategy
  */
 
@@ -18,26 +18,59 @@
 
 
 
-import loan.loan
-
+import loan.Loan
+import loan.Strategy
 
 
 
 object run{
+
+//  def make_pay(loan: Loan, payment: Double): Loan = 
+//    { loan pay payment }
+
+  //def make_payments(loans: List[Loan], strat: Strat,  
+
+
+
   def main(args: Array[String]) {
     println("hello from manager!")
-    val loan1 = new loan(1.2, 233.4, 23)
-    val loan2 = new loan(0.8, 10023, 75)
-    val loan3 = new loan(.5, 3223, 123)
+    val loan1 = new Loan(1.2, 233.4, 23)
+    val loan2 = new Loan(0.8, 10023, 75)
+    val loan3 = new Loan(.5, 3223, 123)
+
+    val loans = loan1::loan2::loan3::Nil
 
 
 
 
 
-    println(loan1.current_balance)
-    loan1.pay(5)
+    // I want there to be one operation of making payments,
+    // but I don't want the strategy to know any more about payments
+    // then it has to...
+    // strategy should read the loan attributes, but not be able to put
+    // send in the function Strategy.minimum as an argument to pay
+    // that would look like:
+    // pay(loans, Strategy.even, 400)
+
+    val pay_min = Strategy.minimum(loans, 0) 
+    val pay_eq  = Strategy.even(loans, 400)
+
+    val new_loans_wmin = pay_min.map{case(a,b) => b make_payment a}
+    val new_weq        = pay_eq.map{case(a,b) => b make_payment a}
+
+
+
+    println(loans.map(_.balance))
+    println(new_loans_wmin.map(_.balance))
+    println(new_weq.map(_.balance))
+
     
-    println(loan1.current_balance)
+
+
+
+
+
+
   }
 }
 
