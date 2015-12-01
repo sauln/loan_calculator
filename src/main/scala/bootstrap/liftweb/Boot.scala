bedf81b0d5e90cfbecea3fb7becb6dbc62383d69
Nathaniel Saul <nat@saulgill.com>
2015-12-01 00:05:52 -0800

    rearranged the folders, integrated lift_basic into entire project

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -43,7 +43,8 @@ class Boot {
 
     // Build SiteMap
     def sitemap = SiteMap(
-      Menu.i("Home") / "index" >> User.AddUserMenusAfter, // the simple way to declare a menu
+      Menu.i("Home") / "index" >>
+      User.AddUserMenusAfter, // the simple way to declare a menu
 
       // more complex because this menu allows anything in the
       // /static path to be visible
