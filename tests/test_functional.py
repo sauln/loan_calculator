3a8a166f40747261e14b954145c14546e9fe2ffa
Nathaniel Saul <nat@saulgill.com>
2016-01-31 17:09:03 -0800

    Adding new tests, restructing template inheritance structure

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -11,10 +11,36 @@ class NewVisitorTest(LiveServerTestCase):
 		self.browser.quit()
 
 	def test_user_goes_loan_calculator_page(self):
-		#see we are going to the app4apps site
+		#see we are going to the Loan Calculator site
 		self.browser.get(self.live_server_url+"/loancalc")
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn("Loan Calculator", header_text)
 
-		#we want to press the button and get a new one each time.
+		#see a place to input a new loan		
+		inputbox_bal = self.browser.find_element_by_id('id_new_balance')
+		self.assertEqual(
+				inputbox_bal.get_attribute('placeholder'),
+				'Enter the balance'
+		)
+
+		inputbox_ir  = self.browser.find_element_by_id('id_interest_rate')
+		self.assertEqual(
+				inputbox_ir.get_attribute('placeholder'),
+				'Enter the interest rate'
+		)
+		inputbox_min = self.browser.find_element_by_id('id_min_payment')
+		self.assertEqual(
+				inputbox_min.get_attribute('placeholder'),
+				'Enter the minimum payment'
+		)
+		
+		
+		
+		
+		#adds item and is redirected to a new page that shows the list
+		#inputbox.send_keys('Buy peacock feathers')
+		#inputbox.send_keys(Keys.ENTER)
+		#edith_list_url = self.browser.current_url
+		#self.assertRegex(edith_list_url, '/lists/.+')
+		#self.check_for_row_in_list_table('1: Buy peacock feathers')
 
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,11 +1,29 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
-
 from loan_calculator.views import loancalc_page
+
+from django.http import HttpRequest
+
+
+
 
 class LoanCalcTest(TestCase):
 	def test_loancalc_url_resolves_to_loancalc_view(self):
 		found = resolve('/loancalc/')
 		self.assertEqual(found.func, loancalc_page)
+
+
+
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = loancalc_page(request)
+
+		self.assertTrue(response.content.strip().startswith(b'<!DOCTYPE html>'),
+			"Actual response = %s"%response.content.strip())
+		self.assertIn(b'The Loan Calculator', response.content)
+		self.assertTrue(response.content.strip().endswith(b'</html>'), 
+			"Actual response = %s"%response.content)
+
+
 
 
