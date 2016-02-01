39d4aae65358ec2574e1ccccd40e1a5e5349c8fa
Nathaniel Saul <nat@saulgill.com>
2016-01-31 16:17:38 -0800

    Added user & loan models and an action items list for frontend

--- models.py
+++ models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+#create model for loan and user
+
+class User(models.Model):
+	name = models.CharField(max_length=120, default="No name")
+
+
+class Loan(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	balance = models.DecimalField(max_digits=25, decimal_places=2)
+	interest_rate = models.DecimalField(max_digits=10, decimal_places=6)
+	minimum_payment = models.DecimalField(max_digits=5, decimal_places=2)
+
