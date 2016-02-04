b62563815764c7b5f81423b1ecba6aea095c65e8
Nathaniel Saul <nat@saulgill.com>
2016-02-03 23:16:17 -0800

    Refactored views, slightly simplified

--- views.py
+++ views.py
@@ -8,17 +8,14 @@ def loancalc_page(request):
 		balance=request.POST["balance"]
 		interest_rate=request.POST["interest_rate"]
 		minimum_payment=request.POST["minimum_payment"]
-		print("Balance: %s\nInterest rate: %s\n Minimum payment: %s"%(balance, interest_rate, minimum_payment))
-		
-		chk = Loan(balance=balance, interest_rate=interest_rate, minimum_payment=minimum_payment)
+		chk = Loan(balance=balance, 
+					interest_rate=interest_rate, 
+					minimum_payment=minimum_payment)
 		chk.save()	
-		#print(loans)
-		loans = Loan.objects.all()
-		print(loans)
 		
-		return redirect('/loancalc/', {'loans': loans})
-
 	loans = Loan.objects.all()
-
 	return render(request, 'loancalc.html', {'loans': loans})
+
+	#loans = Loan.objects.all()
+	#return render(request, 'loancalc.html', {'loans': loans})
 	
